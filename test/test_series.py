05d396fcac8aa5f8f296fb97c8223ff15c2e0a12
WalaaAlrefai <lulusedqi@gmail.com>
2023-05-04 21:54:03 +0000

    first commit

---
+++ test/test_series.py
@@ -0,0 +1,11 @@
+# import pytest
+# from series import fibonacci
+
+# def test_fib_1_equals_1():
+#     assert fibonacci(1) == 1
+
+# def test_fib_2_equals_1():
+#     assert fibonacci(2) == 1
+
+# def test_fib_6_equals_8():
+#     assert fibonacci(6) == 8
